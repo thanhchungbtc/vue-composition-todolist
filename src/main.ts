2d559c03ab2ec1cd0a4c91bd4e33704c2ff4b610
Thanh Chung Bui <thanhchungbtc@gmail.com>
2020-06-21 14:47:38 +0900

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueCompositionApi from "@vue/composition-api";
+import "./assets/styles.scss"
+Vue.use(VueCompositionApi);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <b-row class="mt-5">
+      <b-col cols="12">
+        <FilteredButton/>
+        <h3 class="mb-3 font-weight-bold">Todos</h3>
+        <hr/>
+        <TodoItem v-for="item in items" :key="item.id" :item="item"/>
+        <CreateTodo :create-todo="createTodo" :title="title"/>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import {reactive, toRefs} from "@vue/composition-api";
+  import TodoItem from "@/views/TodoItem";
+  import FilteredButton from "@/views/FilteredButton";
+  import CreateTodo from "@/views/CreateTodo";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  function useTodos() {
+    const state = reactive({
+      items: [
+        {id: 1, title: "Hello World", completed: false},
+        {id: 2, title: "Hello World", completed: false}
+      ],
+      title: "",
+      createTodo: () => {
+        state.items.push({
+          id: state.items.length + 1,
+          title: state.title,
+          completed: false
+        });
+        state.title = "";
+      }
+    });
+    return toRefs(state);
   }
-}
+
+  export default {
+    components: {CreateTodo, FilteredButton, TodoItem},
+    setup() {
+      const {items, title, createTodo} = useTodos();
+
+      return {
+        items,
+        title,
+        createTodo
+      };
+    }
+  };
 </script>
